1293ba71e5b66730e01ed035009b85607d1795b6
Michael Petch <mpetch@gmail.com>
2024-11-21 18:42:07 -0700

    Remove debugging stuff

--- kernel/arch/x86-32/interrupts/idt.c
+++ kernel/arch/x86-32/interrupts/idt.c
@@ -190,7 +190,6 @@ void page_fault_handler(struct Interrupt_registers* r) {
 
 void syscall_handler(struct Interrupt_registers* regs, uint32_t syscall_number)
 {
-    __asm__("xchg %bx, %bx");
     printf("hallo");
 }
 
