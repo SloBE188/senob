ed7e2daa17075fefcc24f3d1199f8a5293b6a562
SloBE188 <nils.burkard@outlook.com>
2024-12-27 15:54:33 +0100

    diffrent tss for every cpu

--- kernel/arch/x86-32/sys/startup.c
+++ kernel/arch/x86-32/sys/startup.c
@@ -29,17 +29,29 @@ static inline void load_tr(uint16_t tss_selector)
     asm volatile("ltr %0" : : "r"(tss_selector));
 }
 
+
+static inline uint32_t get_local_apic_id_cpuid(void) 
+{
+    uint32_t eax, ebx, ecx, edx;
+    __asm__ volatile (
+        "cpuid"
+        : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+        : "a"(1)
+    );
+    // APIC-ID is in the bits 31:24 from ebx
+    return (ebx >> 24) & 0xFF;
+}
+
 void initialize_ap()
 {
 
-    uint32_t apic_id = 2;   //will get that from cpuid, just as an example here
-    //uint32_t ap_tss_selector = (5 + apic_id) << 3;  //*8 for getting the right entry
+    uint32_t apic_id = get_local_apic_id_cpuid();
+    uint32_t ap_tss_selector = (5 + apic_id) << 3;  //*8 for getting the right entry
 
     gdt_flush((uint32_t)&gdt_ptr);
-    //load_tr(ap_tss_selector);
+    load_tr(ap_tss_selector);
 
     idt_flush((uint32_t)&idtr);
-    enable_paging();
     mem_change_page_directory(&kernel_directory);
     while(1){}
 }
