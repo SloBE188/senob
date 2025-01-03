709482b29611462a7731702208ba691235a0c940
SloBE188 <nils.burkard@outlook.com>
2025-01-03 21:24:25 +0100

    reworked this whole toolchain

---
+++ libc/build-newlib/i686-elf/libgloss/config.h
@@ -0,0 +1,41 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* .pushsection/.popsection directives allowed */
+#define HAVE_ASM_POPSECTION_DIRECTIVE 1
+
+/* .previous directive allowed */
+#define HAVE_ASM_PREVIOUS_DIRECTIVE 1
+
+/* Using ELF format */
+#define HAVE_ELF 1
+
+/* Using GNU LD */
+#define HAVE_GNU_LD 1
+
+/* support for section attributes */
+#define HAVE_SECTION_ATTRIBUTES 1
+
+/* Missing syscall names */
+#define MISSING_SYSCALL_NAMES 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libgloss"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libgloss LIBGLOSS_VERSION"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libgloss"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "LIBGLOSS_VERSION"
+
+/* symbol prefix */
+#define __SYMBOL_PREFIX ""
